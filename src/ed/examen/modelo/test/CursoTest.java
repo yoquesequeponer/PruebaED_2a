2ccf29ecb8d10e0839610fe4bad835e8b52c4f98
Alvaro <daw092021@gmail.com>
2021-02-23 17:50:27 +0100

    implementacion test <EliminarAlumno>

---
+++ src/ed/examen/modelo/test/CursoTest.java
@@ -0,0 +1,77 @@
+package ed.examen.modelo.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import ed.examen.modelo.Curso;
+import ed.examen.modelo.Persona;
+
+class CursoTest {
+	Curso c = new Curso();
+	@Test
+	void testEliminarAlumno() {
+		//Eliminar un alumno que no existe
+				int tamanoAntes=c.numeroAlumnos();
+				boolean exceptionlanzada=false;
+				try {
+					c.eliminarAlumno("000000000");
+				} catch (Exception e) {
+					exceptionlanzada=true;
+				}
+				int tamanoDesp=c.numeroAlumnos();
+				assertFalse(exceptionlanzada);
+				assertEquals(tamanoAntes, tamanoDesp);
+
+				//Eliminar un alumno que existe
+				c.aniadirAlumno(new Persona("123456789", "Alvaro", "Fernandez"));
+				tamanoAntes=c.numeroAlumnos();
+				exceptionlanzada=false;
+				try {
+					c.eliminarAlumno("123456789");
+				} catch (Exception e) {
+					exceptionlanzada=true;
+				}
+				tamanoDesp=c.numeroAlumnos();
+				assertFalse(exceptionlanzada);
+				assertEquals(tamanoAntes, tamanoDesp+1);
+				
+				//Parametro menor de 9 caracteres
+				exceptionlanzada=false;
+				try {
+					c.eliminarAlumno("00");
+				} catch (Exception e) {
+					exceptionlanzada=true;
+				}
+				assertTrue(exceptionlanzada);
+				
+				//Parametro mas de 9 caracteres
+				exceptionlanzada=false;
+				try {
+					c.eliminarAlumno("0000000000");
+				} catch (Exception e) {
+					exceptionlanzada=true;
+				}
+				assertTrue(exceptionlanzada);
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");	
+	}
+
+}
---
+++ src/ed/examen/modelo/test/PersonaTest.java
@@ -0,0 +1,44 @@
+package ed.examen.modelo.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void testPersonaStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
